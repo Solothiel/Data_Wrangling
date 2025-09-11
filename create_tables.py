0562ae25a8c42458bd9da31c7ef14fbcca500b6e
Solothiel <dste486@wgu.edu>
2025-09-11 07:49:20 -0600

    completed on create_tables.py
    create main → method → completed
    create_tables → method → completed
    drop_tables → method → completed
    create_database → method → completed

--- create_tables.py
+++ create_tables.py
@@ -36,3 +36,21 @@ def create_tables(cur, conn):
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
+
+def main():
+    """ Drops (if exists) and Creates the sparkify database.
+        Establishes connection with the sparkify database and gets
+        cursor to it.
+        Drops all the tables.
+        Creates all tables needed.
+        Finally, closes the connection.
+    """
+
+    cur, conn = create_database()
+
+    drop_tables(cur,conn)
+    create_tables(cur, conn)
+    conn.close()
+
+if __name__ == "__main__":
+    main()
