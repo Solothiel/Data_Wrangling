e55903eaf3e85607050a2e5802133f33ce886c23
Solothiel <dste486@wgu.edu>
2025-09-12 20:18:55 -0600

    completed readme

--- etl.py
+++ etl.py
@@ -165,8 +165,4 @@ if __name__ == "__main__":
     main()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
